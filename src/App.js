f4f8265a09a07895306bdef442e31753b425ece3
Rajat Subhra Basu <s3951652@student.rmit.edu.au>
2024-05-11 05:17:39 +1000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {Typography, colors} from '@mui/material'
+
 function App() {
   return (
-    <div> MUI Tutorial</div>
+    <div> MUI Tutorial
+      <Typography variant='h1' sx={{color: 'red'}}>
+        Testing Typopgraphy 
+      </Typography>
+      <h1>
+        Dummy text
+      </h1>
+    </div>
   );
 }
 
